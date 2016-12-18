f4913e4d7709849cd44da5173d249e2f8346506b
Stephen <sgornick@digicoast.com>
2016-12-18 15:59:34 -0800

    Initial commit of source.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # usnationaldebtrss
-U.S. National Debt RSS Feed
+Feed of the recent U.S. Public Debt
+
+## Synopsis
+The U.S. Treasury publishes an RSS feed daily with various metrics regarding the debt.   This replicates that feed, but publishes just the Total Public Debt number.
+
+
+## Installation
+This is a Flask app (Python3).
+
+``` sh
+$ cd [your workspace]
+$ git clone https://github.com/pubdata/usnationaldebtrss.git
+$ cd usnationaldebtrss
+$ virtualenv -p python3 venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+``` 
+
+Further deploy instructions will vary based on production system requirements.
+
+For testing, use gunicorn:
+
+``` sh
+$ gunicorn usnationaldebtrss:app
+```
+
+From a browser, access the app using: http://localhost:8000/rss
+
+## Example
+
+This is running at http://www.debttothepenny.com/rss, which is used to publish the <a href="http://twitter.com/debttothepenny">@DebtToThePenny</a> Twitter feed.
+
+## License
+
+MIT
---
+++ usnationaldebtrss.py
@@ -0,0 +1,47 @@
+import feedparser
+from datetime import datetime
+from flask import Flask, request
+from werkzeug.contrib.atom import AtomFeed
+
+
+def feed_entries():
+    results = []
+
+    d = feedparser.parse('http://treasurydirect.gov/NP/debt/rss')
+
+    for entry in d.entries:
+        # The data is published the next business day, but that date is the
+        # date to show, not the "close of business" date which the value is for.
+        entry_pub_date = \
+            datetime.strptime(entry['published'], '%a, %d %b %Y %H:%M:%S GMT')
+        # Parse conent, pulling the number value for 'Public Debt Outstanding'
+        # which happens to be the last part.    E.g.,
+        # "Total Public Debt Outstanding:&lt;/em&gt; 19,443,266,164,413.41"
+        formatted_num_val = entry['content'][0]['value'].split()[-1]
+        entry_text = \
+            "Total Public Debt Outstanding: ${}".format(formatted_num_val)
+        results.append({
+            "pubDate": entry_pub_date,
+            "entryTitle": "{:%Y-%m-%d}".format(entry_pub_date),
+            "entryText": entry_text})
+
+    return results
+
+
+app = Flask(__name__)
+
+
+@app.route('/rss/', methods=['GET'])
+def index():
+    feed = \
+        AtomFeed('Recent entries', feed_url=request.url, url=request.url_root)
+    for entry in feed_entries():
+        feed.add(
+            entry["entryTitle"], entry["entryText"],
+            id='{:%Y%m%d%H%M%S}'.format(entry["pubDate"]),
+            content_type='text',
+            author='U.S. Treasury RSS treasurydirect.gov/NP/debt/current',
+            url='http://treasurydirect.gov/NP/debt/current',
+            updated=entry["pubDate"],
+            published=entry["pubDate"])
+    return feed.get_response()
